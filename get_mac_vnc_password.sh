4d043e5c19bfc73d6a0fffcd86b4664ecd882a38
David Wooldridge (z0mbix) <zombie@zombix.org>
2011-03-22 12:24:33 +0000

    Updated to fix stupid typo and usage

--- get_mac_vnc_password.sh
+++ get_mac_vnc_password.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 #
-# Description: Get the VNC password from MAC OS X
-# Requires: perl
+# Description:
+#   Get the VNC password from MAC OS X
+# Requires:
+#   Perl and permissions to access the VNC password file (root)
+# Usage:
+#   sudo ./get_mac_vnc_password.sh
 #
 FILE=/Library/Preferences/com.apple.VNCSettings.txt
-if [ ${UNAME} = "Darwin" ]; then
+if [ `uname` == "Darwin" ]; then
 	if [ -f $FILE ]; then
 		cat $FILE | perl -wne 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; chomp; @p = unpack "C*", pack "H*", $_; foreach (@k) { printf "%c", $_ ^ (shift @p || 0) }; print "\n"'
 	else
