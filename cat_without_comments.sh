435b2c6a5d5230eba7ed48a7343e861ec21088cd
David Wooldridge (z0mbix) <zombie@zombix.org>
2011-04-06 12:57:24 +0100

    Updated cat without comments script

--- cat_without_comments.sh
+++ cat_without_comments.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+#
+# Description:
+#   cat a file to stdout without #, // or ; comments or blank lines
+#
 
-grep -v -e '^#' $1
+if [ -z $1 ]; then
+	echo "You must specify a file name!"
+	exit 1
+fi
+
+grep -v -E '^#|^;|^$' $1
