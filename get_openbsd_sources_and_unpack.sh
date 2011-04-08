cd8905ef88e90694680e4aeeaa8bbdb4a2cd339c
David Wooldridge (z0mbix) <zombie@zombix.org>
2011-04-08 15:20:30 +0100

    Updated openbsd source/ports grabbing script to run as root

--- get_openbsd_sources_and_unpack.sh
+++ get_openbsd_sources_and_unpack.sh
@@ -1,22 +1,52 @@
 #!/bin/sh
+#
+# Description:
+#   Download OpenBSD sources and ports tree for your release
+# Requirements:
+#   Only what is in base
+# Usage:
+#   sudo ./get_openbsd_sources_and_unpack.sh
+# Notes:
+#   As there are no checksums for these packages, we just do
+#   a simple check to see if the ftp command completed OK
+#   and that the files exist before trying to unpack them.
+# 
 
-if [ `uname` != "OpenBSD" ]; then
-	echo "System is not OpenBSD!! Aborting."
+RELEASE=`uname -r`
+MIRROR=ftp.openbsd.org
+#MIRROR=ftp.plig.net # London, UK Mirror
+URL=ftp://$MIRROR/pub/OpenBSD/$RELEASE
+
+if [ `id -u` -ne 0 ]; then
+	echo "You need to be root to unpack the source and ports tree in /usr"
 	exit 1
 fi
 
-RELEASE=$1
-MIRROR=ftp.openbsd.org
-URL=ftp://$MIRROR/pub/OpenBSD/$RELEASE
+if [ `uname` != "OpenBSD" ]; then
+	echo "System is not OpenBSD!"
+	exit 1
+fi
 
 echo "Getting system source:"
-ftp $URL/src.tar.gz
+ftp -C $URL/src.tar.gz
+if [ $? -ne "0" ]; then
+	echo "System source (src.tar.gz) did not download correctly!"
+	exit 1
+fi
 
 echo "Getting kernel source:"
-ftp $URL/sys.tar.gz
+ftp -C $URL/sys.tar.gz
+if [ $? -ne "0" ]; then
+	echo "Kernal source (sys.tar.gz) did not download correctly!"
+	exit 1
+fi
 
 echo "Getting ports tree:" 
-ftp $URL/ports.tar.gz
+ftp -C $URL/ports.tar.gz
+if [ $? -ne "0" ]; then
+	echo "Ports tree (ports.tar.gz) did not download correctly!"
+	exit 1
+fi
 
 if [ ! -d /usr/src ]; then
 	mkdir /usr/src
